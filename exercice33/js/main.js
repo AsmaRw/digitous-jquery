150cf4dacc0283e128d04cb2f821b7af0ea1c5be
asma <asma.rw09@gmail.com>
2021-04-02 16:43:39 +0200

    jquery

---
+++ exercice33/js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    $('.btn').click(function () {
+        $.ajax({
+            url: 'https://restcountries.eu/rest/v2/name/france',
+
+            success: function (data, statuts, response) {
+                // console.log(data[0].name);
+                // console.log(data[0].capital);
+                $("#country").html(data[0].name)
+                $("#capital").html(data[0].capital)
+            }
+        })
+      
+    })
+
+})
+
